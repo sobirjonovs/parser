a09d9885a6f2cc8c9e6466c2bcc1072299f44c32
Sanjarbek Sobirjonov <sobirjonovsdev@gmail.com>
2023-01-28 09:10:51 +0500

    something happened

--- parser/parse.py
+++ parser/parse.py
@@ -1,5 +1,8 @@
 import random
+from threading import Thread
+
 import requests
+from bs4 import BeautifulSoup
 
 from requests.adapters import HTTPAdapter, Retry
 
@@ -14,6 +17,22 @@ class Parser:
         '139.59.228.95:8118'
     ]
 
+    # Stores keyed with store link and within places to parse
+    STRATEGIES = {
+        'https://asaxiy.uz': {
+            'TITLE': 'div'
+        },
+        'https://olcha.uz': {
+            'TITLE': 'div'
+        },
+        'https://elmakon.uz': {
+            'TITLE': 'div'
+        },
+        'https://texnomart.uz': {
+            'TITLE': 'div'
+        }
+    }
+
     def __init__(self, url: str):
         """
         Initialize a request object using session
@@ -21,6 +40,7 @@ class Parser:
         :rtype: object
         """
         self.url = url
+        self.threads = []
         self.session = requests.session()
         retry = Retry(connect=3, backoff_factor=0.5)
         adapter = HTTPAdapter(max_retries=retry)
@@ -28,6 +48,26 @@ class Parser:
         # mount http/https for url
         self.session.mount('http://', adapter)
         self.session.mount('https://', adapter)
+
+    def parse(self):
+        for store_link, parameters in self.STRATEGIES.items():
+            pass
+
+    def _get(self, url: str, **kwargs):
+        """
+        Type: Protected method
+        Sends get request using session object and close it quickly
+        :return: Response
+        """
+        session = self.session.get(url=url, timeout=self.REQUEST_TIMEOUT, headers=self.HEADERS,
+                                   proxies=self.__generate_proxy(), **kwargs)
+
+        session.close()
+
+        return session
+
+    def _soup(self, url: str, type_: str = 'lxm'):
+        return BeautifulSoup(self._get(url).content, type_)
 
     def __generate_proxy(self):
         """
@@ -37,10 +77,17 @@ class Parser:
         """
         return {'http': random.choice(self.PROXIES)}
 
-    def _get(self, url: str, **kwargs):
+    def __add_thread(self, name, callable_):
         """
-        Type: Protected method
-        Sends get request using session object and close it quickly
-        :return: dict
+        Create a thread and start it
+        :rtype: object
         """
-        return self.session.get(url, **kwargs).close()
+        thread = Thread(target=callable_, args=[name])
+        thread.start()
+        self.threads.append(thread)
+
+        return self
+
+    def __join_threads(self):
+        for thread in self.threads:
+            thread.join()
